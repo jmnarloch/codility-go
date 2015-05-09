6b51ce78bfd7285dbc1fd160c2dde5dab8e309da
Jakub Narloch <jmnarloch@gmail.com>
2015-05-09 23:07:37 +0200

    MaxSliceSum.go

---
+++ src/max_slice_sum/max_slice_sum_test.go
@@ -0,0 +1,16 @@
+package max_slice_sum
+import "testing"
+
+func Test(t *testing.T) {
+
+	// given
+	A := []int {3, 2, -6, 4, 0}
+
+	// when
+	result := Solution(A)
+
+	// then
+	if result != 5 {
+		t.Error("Incorrect result", result)
+	}
+}
