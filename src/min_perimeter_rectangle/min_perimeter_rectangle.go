e3a3a56606b2c506c7e5bc30952d6c754984d447
Jakub Narloch <jmnarloch@gmail.com>
2015-05-10 12:36:25 +0200

    MinPerimeterRectangle.go

---
+++ src/min_perimeter_rectangle/min_perimeter_rectangle.go
@@ -0,0 +1,21 @@
+package min_perimeter_rectangle
+
+func Solution(N int) int {
+	// write your code in Go 1.4
+
+	min := (N + 1) * 2
+	for div := 1; div * div <= N; div++ {
+		if N % div == 0 {
+			min = Min(min, (div + (N / div)) * 2)
+		}
+	}
+	return min
+}
+
+func Min(a, b int) int {
+	if a < b {
+		return a
+	} else {
+		return b
+	}
+}
