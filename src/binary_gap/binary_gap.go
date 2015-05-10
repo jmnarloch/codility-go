c26426e6c2b01b297bb8151f6175135062f843fe
Jakub Narloch <jmnarloch@gmail.com>
2015-05-10 20:54:00 +0200

    BinaryGap.go

---
+++ src/binary_gap/binary_gap.go
@@ -0,0 +1,32 @@
+package binary_gap
+
+func Solution(N int) int {
+	// write your code in Go 1.4
+
+	val := uint(N)
+	for val > 0 && (val & 1) == 0 {
+		val >>= 1
+	}
+
+	gap := 0
+	maxGap := 0
+	for val > 0 {
+		if (val & 1) == 1 {
+			maxGap = Max(maxGap, gap)
+			gap = 0
+		} else {
+			gap++
+		}
+		val >>= 1
+	}
+
+	return Max(maxGap, gap)
+}
+
+func Max(a, b int) int {
+	if a > b {
+		return a
+	} else {
+		return b
+	}
+}
